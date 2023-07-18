301ed89a913338df36ebcc61fdfce54ec37ea113
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-07-18 18:28:55 -0300

    arregle estilos y screen detail para ver imagen

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -8,9 +8,13 @@ function Gif({title, url, id}) {
   return (
 
     <div className='card-gif'>
-      <Link to={`/gif/${id}`}>
-        <h4>{title}</h4>
-        <img className='imagen' alt={title} src={url}/>
+      <Link to={`/gif/${id}`} className='link'>
+        <div className='titleContainer'>
+          <h4>{title}</h4>
+        </div>
+        <div className='imagenContainer'>
+          <img className='imagen' alt={title} src={url}/>
+        </div>
       </Link>
     </div>
 
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -7,28 +7,39 @@ import { API_KEY, API_URL } from '../services/Setting';
 export default function Detail ({params}) {
     const {id} = params
     const [urlImagen, setUrlImagen] = useState("")
+    const [title, setTitle] = useState("")
 
-    // console.log(params);
+    console.log(params);
 
     const estatico = useContext(Context)
-    console.log(estatico);
+    // console.log(estatico);
     
     const {gifs} = useContext(GifsContextProvider)
-    console.log(gifs);
+    // console.log(gifs);
 
     useEffect(() => {
-      fetch(`${API_URL}${id}?${API_KEY}`)
+      // fetch(`${API_URL}${id}?${API_KEY}`)
+      fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=BAo8mj1IAGrHIsi7hOJXNW9i6zhopPBm`)
       .then(respuesta => respuesta.json())
       .then(resp => {
+        console.log(resp);
         setUrlImagen(resp.data.images.downsized.url)
+        setTitle(resp.data.title)
       })
     }, [])
     
 
     return (
-        <div className='detailContainer'>
-            <h1>Gif con id: {id}</h1>
-            <img className='imagenDetalle' alt="imagen" src={urlImagen}/>
+        <div className='container'>
+          <div className='detailContainer'>
+            <div>
+              <h1>{title}</h1>
+              <h4>Gif con id: {id}</h4>
+            </div>
+            <div className='imagenContainer'>
+              <img className='imagenDetalle' alt="imagen" src={urlImagen}/>
+            </div>
+          </div>
         </div>
     )
 
